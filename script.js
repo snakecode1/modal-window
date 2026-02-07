efe4a0810dfdd13b22c23695a4b957dbd65fbd6f
snakecode <snakecode.io@gmail.com>
2026-02-07 09:56:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', closeModal);
